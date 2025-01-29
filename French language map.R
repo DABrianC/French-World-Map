1eea7a0e4a6fdde10a0c2d55d05e31abd8687718
DABrianC <DABrianC@users.noreply.github.com>
2025-01-29 15:12:29 -0500

    BC

--- French language map.R
+++ French language map.R
@@ -1,36 +1,26 @@
 
-remotes::install_github("walkerke/mapgl")
-
 library(tidyverse)
-#library(mapgl)
 library(htmltools)
 library(htmlwidgets)
 library(leaflet)
 library(leaflet.extras)
-#pak::pak("rstudio/leaflet.providers")
 
-#mapgl::maplibre()
 
-library(leaflet.providers)
 
-str(providers_default(), max.level = 2)
-
-url <- "https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
-url1 <- "https://{s}.tile.openstreetmap.en/osmen/{z}/{x}/{y}.png"
-
-leaflet() |> 
+map <- leaflet() |> 
   addTiles(group = "OpenStreepMap") |> 
   addProviderTiles("OpenStreetMap.France"
                    , group = "Open Street Map French") |> 
   addProviderTiles(providers$Esri.WorldImagery
                    , group = "Satellite") |>
-  setView(lng = 90, lat = 50, zoom = 2) |> 
+  setView(lng = 120, lat = 50, zoom = 2) |> 
   addLayersControl(
     baseGroups = c("OpenStreepMap", "OpenStreetMap French", "Satellite")
     , position = "topright"
     , options = layersControlOptions(collapsed = FALSE)
   ) |> 
 addControl(
-  html = "<b>Carte interactive de l'Asie</b>",  # HTML title in French
+  html = "<b>Carte Interactive de l'Asie</b>",  # HTML title in French
   position = "topleft")
 
+saveWidget(map, file = "./Carte_Asie.html", selfcontained =  TRUE)
